2bd1019b850694ffb95cfcbbde44054d0edc8e46
rinkeshpatel22 <rinkeshpatel22@gmail.com>
2019-04-19 17:56:26 +0530

    added comments

--- app/routes/app-routes.js
+++ app/routes/app-routes.js
@@ -6,7 +6,8 @@ const sharedController = require('./../controllers/sharedController');
 // set routes with middlewares and contollers. 
 let setRouter = (app) => {
     app.get('/read', readFileController.readFile);
-    app.get('/write/:data', writeFileController.writeFile);
+    app.post('/write/:data', writeFileController.writeFile); // execute POST call to write from tools like postman
+    app.get('/write/:data', writeFileController.writeFile); // execute GET call to write from browser for demo - idealy it should be POST call.
     app.get('*', sharedController.generateMessage);
 }
 
